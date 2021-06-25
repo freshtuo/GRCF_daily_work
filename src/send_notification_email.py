d86023379f89869b73a8af63d39c4a7be4177114
Tuo Zhang <taz2008@med.cornell.edu>
2021-06-25 10:59:29 -0400

    add support for both sftp and aws by introducing a location argument; reformat the email main text with html

--- src/send_notification_email.py
+++ src/send_notification_email.py
@@ -21,9 +21,11 @@ from email.message import EmailMessage
 from argparse import ArgumentParser
 
 # functions
+# get current time
 def get_time():
 	return '['+asctime(localtime(time()))+']'
 
+# extract information out of the setting yaml file
 def parse_settings(setting_file):
 	# read in setting info
 	with open(setting_file, 'r') as fset:
@@ -38,26 +40,85 @@ def parse_settings(setting_file):
 				sys.exit(1)
 	return(setdic) 
 
-def prepare_email(setting_file):
+# prepare email main text based on data location
+def prepare_content(setdic, location):
+	content = ''
+	if location == 'sftp':
+		# collect info
+		instrument = setdic['run']['instrument']
+		ilab = setdic['run']['iLab']
+		nsamples = setdic['run']['nSamples']
+		dataloc = setdic['user']['dataPath']
+		username = setdic['user']['name']
+		sender = setdic['core']['name']
+		# update text
+		content = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi {},</p>
+    <p>Your {} sequencing run ({} samples, iLab request: {}) is complete. 
+       The data have been uploaded to your lab folder on our internal data transferring server, 
+       which enables both ssh and sftp access with your Cornell CWID.</p>
+    <p>Here are the account information:</p>
+    <p>ssh(sftp) CWID@157.139.241.40<br>
+       Account: CWID<br>
+       Password: password associated with CWID</p>
+    <p>The data can be downloaded from the following directories:</p>
+    <p style="color:blue;font-size:18px;"><b>{}</b></p>
+    <p>You can certainly use terminal to access it but a ftp client like Filezilla (https://filezilla-project.org/) might make your life easier.</p>
+    <p><b>The data will be available for 4 weeks, after that they will be removed from the server. 
+          Please download your data on time.</b></p>
+    <p>The run summary was attached. Please let us know if there are any problems.</p>
+    <p>Best,</p>
+    <p>{}</p>
+  </body>
+</html>
+""".format(username,instrument,nsamples,ilab,dataloc,sender)
+	elif location == 'aws':
+		# collect info
+		instrument = setdic['run']['instrument']
+		ilab = setdic['run']['iLab']
+		nsamples = setdic['run']['nSamples']
+		dataloc = setdic['user']['dataPath']
+		username = setdic['user']['name']
+		sender = setdic['core']['name']
+		# update text
+		content = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi {},</p>
+    <p>Your {} sequencing run ({} samples, iLab request: {}) is complete.
+       The data can be downloaded via the following AWS s3 URL:</p>
+    <p style="color:blue;font-size:18px;"><b>{}</b></p>
+    <p>You can either download the data with a regular web browser by clicking the above link 
+       or through a Linux terminal with a command like this one: </p>
+    <p>wget -O YOU-PREFERED-FILE-NAME.tar  “THE-ABOVE-URL"</p>
+    <p><b>The data will be available for 7 days only, after that they will be removed. Please download your data on time.</b></p>
+    <p>The run summary was attached. Please let us know if there are any problems.</p>
+    <p>Best,</p>
+    <p>{}</p>
+  </body>
+</html>
+""".format(username,instrument,nsamples,ilab,dataloc,sender)
+	return content
+
+def prepare_email(setting_file, location):
 	# parse settings
 	setdic = parse_settings(setting_file)
 	# collect info
 	instrument = setdic['run']['instrument']
 	date = setdic['run']['date']
 	libtype = setdic['run']['libType']
-	ilab = setdic['run']['iLab']
-	nsamples = setdic['run']['nSamples']
 	seqtype = setdic['run']['seqType']
 	readlen = '+'.join(['{}'.format(x) for x in setdic['run']['readLen']])
 	demuxsum = []
 	if setdic['attachments'] is not None:
 		demuxsum = setdic['attachments']['demuxSum']
-	dataloc = setdic['user']['dataPath']
-	username = setdic['user']['name']
 	e_tos = [setdic['user']['email'], setdic['user']['piEmail']]
 	e_ccs = setdic['core']['ccEmails']
 	e_from = setdic['core']['fromEmail']
-	sender = setdic['core']['name']
 	# create email message
 	msg = EmailMessage()
 	msg['Subject'] = '{} {}{} {} {} sequencing data'.format(instrument,seqtype,readlen,date,libtype)
@@ -65,30 +126,8 @@ def prepare_email(setting_file):
 	msg['Cc'] = ', '.join(e_ccs)
 	msg['From'] = e_from
 	# email content
-	msg.set_content("""\
-Hi {},
-
-Your {} sequencing run ({} samples, iLab request: {}) is complete. \
-The data have been uploaded to your lab folder on our internal data transferring server, \
-which enables both ssh and sftp access with your Cornell CWID.
-
-Here are the account information: 
-ssh(sftp) CWID@157.139.241.40 
-Account: CWID 
-Password: password associated with CWID 
-The data can be downloaded from the following directories: 
-
-{}
-
-You can certainly use terminal to access it but a ftp client like Filezilla (https://filezilla-project.org/) might make your life easier. 
-
-The data will be kept there for about one month. 
-
-The run summary was attached. Please let us know if there are any problems. 
-	 
-Best, 
-{}
-""".format(username,instrument,nsamples,ilab,dataloc,sender))
+	content = prepare_content(setdic, location)
+	msg.set_content(content, subtype='html')
 	# add demux summary if exists
 	for dfile in demuxsum:
 		ctype, encoding = mimetypes.guess_type(dfile)
@@ -101,8 +140,8 @@ Best,
 			msg.add_attachment(fp.read(), maintype=maintype, subtype=subtype, filename=basename(dfile))
 	return msg
 
-def send_email(setting_file):
-	msg = prepare_email(setting_file)
+def send_email(setting_file, location):
+	msg = prepare_email(setting_file, location)
 	# Send the message via local SMTP server.
 	with smtplib.SMTP('localhost') as s:
 		s.send_message(msg)
@@ -110,9 +149,10 @@ def send_email(setting_file):
 def get_arguments():
 	parser = ArgumentParser(description="""Send a notification email to user""")
 	parser.add_argument('-s', '--setting', required=True, help="""a yaml file specifying detailed information.""")
+	parser.add_argument('-l', '--location', choices=['sftp','aws'], default='sftp', help="""data location either sftp or aws.""")
 	return parser.parse_args()
 
 if __name__ == '__main__':
 	args = get_arguments()
-	send_email(args.setting)
+	send_email(args.setting, args.location)
 
